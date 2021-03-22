f8744d19977dc2bddc56e4d51125d912cffad93c
Brent B <brentbaughan1@gmail.com>
2021-03-22 00:10:00 -0400

    boilerplated function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,5 @@ const generateDateTime = Date.now();
 //To use the OneCall API, which provides the most information in the fewest API calls, 
 //the user's input must be filtered and converted to lat/long.
 function convertFromName(searchTerm) {
-
+    
 };
