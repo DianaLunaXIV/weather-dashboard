ce3ddf30dfd5a01968be69c60768a0e8ff5c5e4a
Brent B <brentbaughan1@gmail.com>
2021-03-22 00:09:16 -0400

    added json values as javascript object

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//Generate required unix time for API call
+const generateDateTime = Date.now();
+
+//To use the OneCall API, which provides the most information in the fewest API calls, 
+//the user's input must be filtered and converted to lat/long.
+function convertFromName(searchTerm) {
+
+};
