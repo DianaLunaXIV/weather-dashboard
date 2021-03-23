de13a08c7628e7ff8a225709bd6db4433d8df918
Brent B <brentbaughan1@gmail.com>
2021-03-22 21:52:38 -0400

    added jQuery autocomplete functionality and created working API call function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,29 @@
 //Generate required unix time for API call
 const generateDateTime = Date.now();
+let currentCity = {};
+//Use OpenWeather's Geocoding API to filter search terms and convert them to lat,long. Limit to US only for now.
+$( function() {
+    //This is the jQuery Autocomplete source array.
+    // const availableMatches = citiesList.map(city => `${city.name}, ${city.state}`);
+    const availableMatches = citiesList.map(city => ({
+        ...city,
+        value: `${city.name}, ${city.state}`
+    }))
+    $("#citySearchBar").autocomplete({
+        source: availableMatches,
+        select: function(event, ui) {
+            console.log(event, ui.item.coord.lon);
+            currentCity.lon = ui.item.coord.lon;
+            console.log(event, ui.item.coord.lat);
+            currentCity.lat = ui.item.coord.lat;
+        }
+    })
+});
 
-//To use the OneCall API, which provides the most information in the fewest API calls, 
-//the user's input must be filtered and converted to lat/long.
-function convertFromName(searchTerm) {
-    
+function getFromOneCall(){
+    let requestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentCity.lat}&lon=${currentCity.lon}&units=imperial&appid=dae07aaca7616262277cefcd84b42b42`;
+    fetch(requestURL)
+        .then(response => response.json())
+        .then(data => console.log(data));
+
 };
