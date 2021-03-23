53044cdceafbfa1ee938183a0e9f2ec3e8d2b70a
Brent B <brentbaughan1@gmail.com>
2021-03-22 22:18:43 -0400

    first attempt to refactor with fetch call rather than hosted JSON; currently returns error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
 //Generate required unix time for API call
 const generateDateTime = Date.now();
+let searchInputValue = $('#citySearchBar').val()
 let currentCity = {};
+
+function geocodingFetch(){
+    let requestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInputValue},US&limit=20&appid=dae07aaca7616262277cefcd84b42b42`;
+    let returnedJSON;
+    fetch(requestURL)
+        .then(response => response.json())
+        .then(data => JSON.parse(data).map(returnedJSON));
+    return returnedJSON;
+};
+
 //Use OpenWeather's Geocoding API to filter search terms and convert them to lat,long. Limit to US only for now.
 $( function() {
     //This is the jQuery Autocomplete source array.
     // const availableMatches = citiesList.map(city => `${city.name}, ${city.state}`);
-    const availableMatches = citiesList.map(city => ({
-        ...city,
-        value: `${city.name}, ${city.state}`
-    }))
+    const availableMatches = [];
+    
+    
     $("#citySearchBar").autocomplete({
         source: availableMatches,
         select: function(event, ui) {
