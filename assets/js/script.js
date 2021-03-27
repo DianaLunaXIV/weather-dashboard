c5cf9571442fe625105c2183cbae9989abfeccc4
Brent B <brentbaughan1@gmail.com>
2021-03-27 12:01:21 -0400

    removed old, unused code for grabbing onecall info

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,14 +25,8 @@ $( function() {
 
 //Use the current search term to search the api for weather conditions
 let requestedWeather;
-function getFromOneCall(){
-    let requestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentCity.lat}&lon=${currentCity.lon}&units=imperial&appid=dae07aaca7616262277cefcd84b42b42`;
-    return fetch(requestURL)
-        .then(response => response.json())
-        .then(data => requestedWeather = data);
-};
 
-// yours works but this is another way to do it
+//Use OneCall API asynchronously to retrieve weather conditions for the current city
 async function asyncGetFromOneCall(){
     let requestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentCity.lat}&lon=${currentCity.lon}&units=imperial&appid=dae07aaca7616262277cefcd84b42b42`;
     const response = await fetch(requestURL);
@@ -56,7 +50,6 @@ function appendSearchResult(){
 
 async function asyncCall(){
     $('mainCurrentTemperature').text('Loading...');
-    // const result = await getFromOneCall();
     await asyncGetFromOneCall();
     appendMainWeatherData();
     appendSearchResult();
